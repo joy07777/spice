5b0329d3ae8525e6d75c184e516814181089a01a
joy07777 <pearljoyantecristo006@gmail.com>
2024-04-20 13:40:32 +0800

    patooo

--- login.php
+++ login.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joy's Clothing Store</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            background: linear-gradient(to bottom, white, #6495ED);
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+        /* Additional styling for the content */
+        h1 {
+            color: white;
+            text-align: center;
+            margin-top: 50px; /* Adjust as needed */
+        }
+    </style>
+</head>
+<body>
 <?php
 include("header.php");
 include("navbar.php");
